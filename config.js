b4e5fa6d502c427dd2414331f66cd433e234cbb2
mert metin <ksulyvio@hotmail.com>
2023-12-26 23:49:39 +0300

    nodejsproje

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "mert12345",
+        database: "nodedb"
+    }
+}
+
+module.exports = config;
---
+++ data/db.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+const config = require("../config");
+
+let connection = mysql.createConnection(config.db);
+
+connection.connect(function(err){
+    if(err) {
+        console.log(err);
+    }
+    console.log("mysql bağlantisi yapildi.");
+});
+    
+module.exports = connection.promise();
